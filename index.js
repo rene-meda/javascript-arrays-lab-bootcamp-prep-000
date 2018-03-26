e363eaa8671579c5670512785d6f19fec978220e
Adolpho Noguera <adolph518@gmail.com>
2018-03-26 03:46:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const app = "I don't do much."
 
 function destructivelyAppendKitten(name){
-  //var kittens = ["Milo", "Otis", "Garfield"]
-  kittens.push(name) 
-  return kittens
+  return kittens.push(name)
 }
